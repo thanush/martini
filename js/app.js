7ced2f33c0b185bf96901af9d4c83b8cb2fb6b44
thanush <beingthanush@gmail.com>
2016-11-03 17:30:19 +0530

    some changes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
 $(function() {
 	$(".stat-name").lettering();
-	$('.owl-carousel').owlCarousel({
+	$('.about-slider').owlCarousel({
 	    loop:true,
 	    margin:10,
 	    items:1,
 	    nav: true,
 	    navText: ["<span class='glyphicon glyphicon-chevron-left'></span>","<span class='glyphicon glyphicon-chevron-right'></span>"]
 	});
+	$('.testi-slider').owlCarousel({
+	    autoplay: true,
+		center: true,
+		loop: true,
+		nav: true,
+	    items:3,
+	    nav: true,
+	    navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"]
+	})
 	$('.feat-item').hover(function(){
 		$(this).find('.feat-hcard').addClass('hcardopen')
 	},
 	function(){
 		$(this).find('.feat-hcard').removeClass('hcardopen')
+	})
+	$('.team-member').hover(function(){
+		$(this).find('.team-info').addClass('tcardopen')
+	},
+	function(){
+		$(this).find('.team-info').removeClass('tcardopen')
 	})
 });
